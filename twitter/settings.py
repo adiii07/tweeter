776b625f8a8c1bde4e1e14a5afb7ba2cf0536f8e
Aditya Balasubramanian <adityabala2005@gmail.com>
2021-12-27 16:57:56 +0530

    used whitenoise to serve static files

--- twitter/settings.py
+++ twitter/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'twitter.urls'
@@ -131,3 +132,5 @@ STATICFILES_DIRS =  (
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_COOKIE_SECURE = True
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
